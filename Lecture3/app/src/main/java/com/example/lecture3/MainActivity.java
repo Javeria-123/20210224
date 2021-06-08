d5f1de48a9871ff443338f9492016b4077a81bf8
javeria-123 <mcsf19m048@pucit.edu.pk>
2021-06-08 12:56:59 -0700

    Intents

---
+++ Lecture3/app/src/main/java/com/example/lecture3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.lecture3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void viewWebsite(View view) {
+        Uri uri= Uri.parse("https://youtube.com");
+        Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
+    public void call(View view) {
+        Uri uri= Uri.parse("tel:");
+        Intent intent=new Intent(Intent.ACTION_DIAL,uri);
+        startActivity(intent);
+    }
+}
