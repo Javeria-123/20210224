b7ca1bb107ca9d20523c453691a45ba028c67a7a
javeria-123 <mcsf19m048@pucit.edu.pk>
2021-06-10 04:21:23 -0700

    moving activity from audio to video

--- Animmations/app/src/main/java/com/example/animmations/Audio.java
+++ Animmations/app/src/main/java/com/example/animmations/Audio.java
@@ -2,6 +2,7 @@ package com.example.animmations;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -16,14 +17,23 @@ public class Audio extends AppCompatActivity {
         setContentView(R.layout.activity_audio);
 
         mp=MediaPlayer.create(this,R.raw.audio);
-       
+
+
     }
 
     public void play(View view) {
+
+
         mp.start();
     }
 
     public void stop(View view) {
+
         mp.stop();
+    }
+
+    public void playVideo(View view) {
+        Intent intent=new Intent(this,Vedio_activity.class);
+        startActivity(intent);
     }
 }
